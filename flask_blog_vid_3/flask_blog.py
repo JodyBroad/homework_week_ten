826c2a41af62097cb20abf272f7939ddbade2d85
Jody Broad <msjodybroad@gmail.com>
2022-04-09 18:14:21 +0100

    jody doing vid 3 in corey schaffer series covering forms

--- flask_blog_vid_3/flask_blog.py
+++ flask_blog_vid_3/flask_blog.py
@@ -1,7 +1,11 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import RegistrationForm, LoginForm
 
 flask_blog_app = Flask(__name__)
 
+flask_blog_app.config['SECRET_KEY'] = 'e0b3b359461fab01e70f38bbee9045e9'
+# import os
+# flask_blog_app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 
 # in reality we would be calling this info from a database
 posts = [
@@ -28,6 +32,26 @@ def home():
 @flask_blog_app.route('/about')
 def about():
     return render_template('about.html', title='About')
+
+@flask_blog_app.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('register.html', title='Register', form=form)
+
+@flask_blog_app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        # fake login for now as we don't have database connection yet
+        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login unsuccessful, please check username and password', 'danger')
+    return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == "__main__":
