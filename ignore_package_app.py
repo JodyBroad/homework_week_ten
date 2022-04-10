b61633c13baad66f903f32698fcd2f094cb92c65
Jody Broad <msjodybroad@gmail.com>
2022-04-10 12:11:59 +0100

    aborting the package attempt

---
+++ ignore_package_app.py
@@ -0,0 +1,11 @@
+# requirements.txt will have all the things you need to install to run your code
+
+#ignore this file, abortive attempt to convert to package
+
+# import the flask object instance we just instantiated
+# from nail_bar_package import app
+
+
+# main trick
+if __name__ == "__main__":
+    app.run(debug=True, port="4003")
